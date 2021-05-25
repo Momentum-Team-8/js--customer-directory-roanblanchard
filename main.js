f0e881dd6595051716770758a36f8fb4e2efc3a0
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-25 16:14:56 -0400

    getting names and pictures to display via example

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 
+let customerFirstNames = customers.map((cus) => cus.name.first)
+let customerImages = customers.map((cus) => cus.picture.medium)
+
+
+
+function renderNames(arr) {
+    // scoutFirstNames.forEach()
+    arr.forEach(name => {
+        let h2 = document.createElement("h2")
+        h2.innerText = name;
+        document.getElementById("output").appendChild(h2)
+    });
+}
+
+function renderImages(arr) {
+    arr.forEach(image => {
+        let pic = document.createElement("img")
+        pic.src = image
+        document.getElementById("output").appendChild(pic)
+    })
+}
+
+renderNames(customerFirstNames)
+renderImages(customerImages)
