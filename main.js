940f9915023b9c64d76a05e272c6a8bf03c60a19
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-25 20:17:56 -0400

    added customer address

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for (let x of customers) {
     const customerImg = document.createElement('img')
     customerImg.src = x.picture.large
     customerCard.appendChild(customerImg)
-    // grabbing and redndering the name
+    // grabbing and rendering the name
     const customerName = document.createElement('h2')
     customerName.innerText = x.name.first + ' ' + x.name.last
     customerCard.appendChild(customerName)
@@ -19,4 +19,12 @@ for (let x of customers) {
     const customerEmail = document.createElement('p')
     customerEmail.innerText = x.email
     customerCard.appendChild(customerEmail)
+    // customer's address
+    const customerAdress1 = document.createElement('p')
+    customerAdress1.innerText = x.location.street.number + ' ' + x.location.street.name
+    customerCard.appendChild(customerAdress1)
+    const customerAdress2 = document.createElement('p')
+    const state = nameToAbbr(x.location.state)
+    customerAdress2.innerText = x.location.city + ' ' + state + ' ' + x.location.postcode
+    customerCard.appendChild(customerAdress2)
 }
